365a99b81c5805e3a11b4070bf9223b844c4df62
John <john@synergitech.co.uk>
2020-11-27 15:02:43 +0000

    Improve basic template

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,11 +1,29 @@
-<!-- Required meta tags -->
+
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 <meta name="description" content="{{ $page->meta_description }}">
 <meta name="keywords" content="{{ $page->meta_keywords }}">
 <meta name="title" content="{{ $page->meta_title }}">
+<!-- meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors"-->
+<title>{{ $title }}</title>
 
 <!-- Bootstrap CSS -->
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
 
-<title>{{ $title }}</title>
+<style>
+  .bd-placeholder-img {
+    font-size: 1.125rem;
+    text-anchor: middle;
+    -webkit-user-select: none;
+    -moz-user-select: none;
+    -ms-user-select: none;
+    user-select: none;
+  }
+
+  @media (min-width: 768px) {
+    .bd-placeholder-img-lg {
+      font-size: 3.5rem;
+    }
+  }
+</style>
+<!-- Custom styles for this template -->
--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -3,19 +3,25 @@
 <head>
     @include('includes.header')
 </head>
-  <body>
-      @include('includes.navbar')
-      <div class="container-fluid">
-          <div class="row">
-              <div class="col-1">
-                  @include('includes.sidebar')
-              </div>
-              <div class="col-11">
-                  <!-- Content here -->
-                  @yield('content')
-              </div>
+<body>
+    @include('includes.navbar')
+    @include('includes.sidebar')
+    <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-md-4">
+      <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+        <h1 class="h2">Dashboard</h1>
+        <div class="btn-toolbar mb-2 mb-md-0">
+          <div class="btn-group mr-2">
+            <button type="button" class="btn btn-sm btn-outline-secondary">Share</button>
+            <button type="button" class="btn btn-sm btn-outline-secondary">Export</button>
           </div>
-    </div>
-    @include('includes.footer')
-  </body>
+          <button type="button" class="btn btn-sm btn-outline-secondary dropdown-toggle">
+            <span data-feather="calendar"></span>
+            This week
+          </button>
+        </div>
+      </div>
+      @yield('content')
+</main>
 </html>
+@include('includes.footer')
+</body>
