2269bc94967b3fb3e5de1763428d46bc44b565eb
John <john@synergitech.co.uk>
2020-11-27 14:10:48 +0000

    Basic front end

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,5 +1,11 @@
-<title>{{ $title }}</title>
-
+<!-- Required meta tags -->
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 <meta name="description" content="{{ $page->meta_description }}">
 <meta name="keywords" content="{{ $page->meta_keywords }}">
 <meta name="title" content="{{ $page->meta_title }}">
+
+<!-- Bootstrap CSS -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+
+<title>{{ $title }}</title>
--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,12 +1,21 @@
-<html>
+<!doctype html>
+<html lang="en">
 <head>
-  @include('includes.header')
+    @include('includes.header')
 </head>
-<body>
-
-  @yield('content')
-
-  @include('includes.footer')
-
-</body>
+  <body>
+      @include('includes.navbar')
+      <div class="container-fluid">
+          <div class="row">
+              <div class="col-1">
+                  @include('includes.sidebar')
+              </div>
+              <div class="col-11">
+                  <!-- Content here -->
+                  @yield('content')
+              </div>
+          </div>
+    </div>
+    @include('includes.footer')
+  </body>
 </html>
